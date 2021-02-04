2d7d02f1be9702920f001bbbf81552303d0dcc4f
Vinicius Schettino <schettino.viniciusjunqueira@gmail.com>
2021-02-04 14:07:18 -0300

    use custom AMI

--- cluster.ts
+++ cluster.ts
@@ -24,7 +24,7 @@ export class ECSCluster extends Stack {
       vpc,
       containerInsights: true
      });
-    const ami = new LookupMachineImage({name: "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"}) // 20201026
+    const ami = new LookupMachineImage( {name: "passeidireto-ecs-sysbox*"})
     const asg = cluster.addCapacity('gh-runner-automanaged', {
       instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.XLARGE),
       machineImage: ami,
@@ -41,51 +41,8 @@ export class ECSCluster extends Stack {
     }); 
     asg.addUserData(
       "sudo -s",
-      "export TZ=America/Sao_Paulo",
-      "apt-get update",
-      "apt-get -y install \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        wget \
-        jq \
-        software-properties-common \
-        git \
-        make \
-        dkms",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -",
-      "apt-key fingerprint 0EBFCD88",
-      "add-apt-repository \
-        \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable\"",
-      "apt-get update",
-      "apt-get -y install docker-ce docker-ce-cli containerd.io",
-      "git clone https://github.com/toby63/shiftfs-dkms.git shiftfs-dkms",
-      "cd shiftfs-dkms && make -f Makefile.dkms",
-      "modprobe shiftfs",
-      "lsmod | grep shiftfs",
-      "service docker start",
-      "echo '{\"default-runtime\": \"sysbox-runc\", \"runtimes\": { \"sysbox-runc\": { \"path\": \"/usr/local/sbin/sysbox-runc\" } } }' | jq '.' > /etc/docker/daemon.json",
-      "git config --system url.https://github.com/.insteadOf git@github.com:",
-      "git clone --recursive git@github.com:nestybox/sysbox.git",
-      "cd sysbox",
-      "make sysbox",
-      "make install",
-      "scr/sysbox",
-      "service docker restart",
-      "usermod -aG docker ubuntu",
-      "sh -c \"echo 'net.ipv4.conf.all.route_localnet = 1' >> /etc/sysctl.conf\"",
-      "sysctl -p /etc/sysctl.conf",
-      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections",
-      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections",
-      "apt-get -y install iptables-persistent",
-      "iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679",
-      "iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679",
-      "iptables -A INPUT -i eth0 -p tcp --dport 51678 -j DROP",
-      "sh -c 'iptables-save > /etc/iptables/rules.v4'",
-      "mkdir -p /etc/ecs && touch /etc/ecs/ecs.config",
+      "/usr/local/sbin/sysbox",
+      "docker restart",
       `echo ECS_CLUSTER=${cluster.clusterName} | tee /etc/ecs/ecs.config`,
       "echo ECS_DATADIR=/data | tee -a /etc/ecs/ecs.config",
       "echo ECS_ENABLE_TASK_IAM_ROLE=true | tee -a /etc/ecs/ecs.config",
